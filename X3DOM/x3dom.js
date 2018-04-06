27576429b0db03749467ac1271cfec3eaca0bdbf
a15filba <a15filba@student.his.se>
2018-04-06 11:32:26 +0200

    Store coordinates and an extra value to an array

--- X3DOM/x3dom.js
+++ X3DOM/x3dom.js
@@ -1,10 +1,10 @@
 
-var coordinates = []; // Array to hold coordinate id
+var coordinates = {}; // Array to hold coordinate id
 
 function setCoordinate(){ // Change the coordinates in the scene
     var oldCoordinate = document.getElementById('coordinateID').getAttribute('point');
     console.log('The old value was ' + oldCoordinate);    
-    var setNewCoordinate = document.getElementById('coordinateID').setAttribute('point', '3 0 0, 5 0 0, 4 2 0');
+    //var setNewCoordinate = document.getElementById('coordinateID').setAttribute('point', '0 0 0, 2 0 0, 1 1 0');
     
     var newCoordinate = document.getElementById('coordinateID').getAttribute('point');
     console.log('The new value is ' + newCoordinate);
@@ -15,10 +15,10 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var jsonData = JSON.parse(this.responseText);
-       
+
         var dotQuantity = 0;
 
-        function newCoordinate(){
+        function newCoordinate() {
             var latCoordinate = jsonData[dotQuantity]["Provets latitud (DD)"];
             var longCoordinate = jsonData[dotQuantity]["Provets longitud (DD)"];
             var newLat = latCoordinate.replace(",","."); // Replace any commas with a dot to be able to render the coordinate
@@ -27,15 +27,22 @@ xmlhttp.onreadystatechange = function() {
             addID(newLat, newLong);
         };
 
-        function addID(x, y){
-            var id = y + x * 600;
-            coordinates.push(id);
+        function addID(x, y) {
+            var id = y + x * 600; // 600 is the width of the canvas
+            
+            if(coordinates[id]) {
+                coordinates[id] += 100;
+            } else {
+                coordinates[id] = 100;
+            }
+
         };
 
         for(dotQuantity; dotQuantity < jsonData.length; dotQuantity++){
             newCoordinate();
         }
 
+        // console.log(jsonData.length)
         console.log(coordinates);
     }
 };
