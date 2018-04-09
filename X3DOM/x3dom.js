4f42a8a0a52b61c584311ac07488dd932ad36434
a15filba <a15filba@student.his.se>
2018-04-09 17:18:35 +0200

    Can now convert coordinates and calculate how "hot" an area should be.

--- X3DOM/x3dom.js
+++ X3DOM/x3dom.js
@@ -1,5 +1,4 @@
-
-var coordinates = {}; // Array to hold coordinate id
+'use strict';
 
 function setCoordinate(){ // Change the coordinates in the scene
     var oldCoordinate = document.getElementById('coordinateID').getAttribute('point');
@@ -16,34 +15,98 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var jsonData = JSON.parse(this.responseText);
 
+        // Objects to hold coordinate id and value id
+        var coordinates = {}; 
+        var values = {};
+        
         var dotQuantity = 0;
+        // Source coordinates will be in Longitude/Latitude, WGS84
+        var source = new proj4('EPSG:4326');
 
-        function newCoordinate() {
-            var latCoordinate = jsonData[dotQuantity]["Provets latitud (DD)"];
-            var longCoordinate = jsonData[dotQuantity]["Provets longitud (DD)"];
-            var newLat = latCoordinate.replace(",","."); // Replace any commas with a dot to be able to render the coordinate
-            var newLong = longCoordinate.replace(",",".");
+        // Destination coordinates in meters, global spherical mercators projection
+        var dest = new proj4('EPSG:3785');
 
-            addID(newLat, newLong);
+        var canvasWidth = document.getElementById('x3d').getAttribute('width');
+        var width = Number(canvasWidth.replace("px",""));
+
+        function prepareCoordinates() {
+            var coords = newCoordinate();
+            var point = convertLatlon(coords.lat, coords.lon);
+
+            addID(point);
         };
 
-        function addID(x, y) {
-            var id = y + x * 600; // 600 is the width of the canvas
-            
-            if(coordinates[id]) {
-                coordinates[id] += 100;
-            } else {
-                coordinates[id] = 100;
-            }
+        function newCoordinate() {
+            // Replace any commas with a dot to be able to render the coordinate
+            var lat = jsonData[dotQuantity]["Provets latitud (DD)"].replace(",",".");
+            var lon = jsonData[dotQuantity]["Provets longitud (DD)"].replace(",",".");
+            return {lat: lat, lon: lon};
+        };
 
+        // Returns a point with x and y coordinates in meters
+        function convertLatlon(lat, lon) {
+            var point = proj4.toPoint([lon, lat, 0.0]);
+            return proj4.transform(source.oProj, dest.oProj, point);
+        };
+
+        function addID(point) { // Adds the coordinates and a value to an array
+            var id = ((point.x + point.y) % jsonData.length) + "";
+            var radius = 0;
+            // x and y coordinates in 100km instead of meters
+            var x = point.x / 100000;
+            var y = point.y / 100000;
+
+            // Move the coordinates closer to origo
+            var movedX = x - 15;
+            var movedY = y - 85;
+
+            // If coordinates has property of id, increase radius
+            if(coordinates[id]) {
+                coordinates[id].radius += 1;
+            } else {
+                // Create new coordinate
+                coordinates[id] = {radius : 1, x : movedX, y : movedY};
+            }
+        };
+
+        function render(){
+            for(var id in coordinates) {
+                var radius = coordinates[id].radius;
+
+                // Retrieve the x and y coordinate
+                var xpoint = coordinates[id].x;
+                var ypoint = coordinates[id].y
+                
+                // If x are within the radius, run loop
+                for(var x = xpoint - radius; x < xpoint + radius; x++) {
+                    for(var y = ypoint - radius; y < ypoint + radius; y++) {
+                        var distance = Math.sqrt(Math.pow((x - xpoint), 2) + Math.pow((y - ypoint), 2));
+                        if(distance > radius) {
+                            continue;
+                        } else {
+                            var value = radius - 1 * distance;
+                            // retrieve the ID for the coordinate
+                            var id = x + y;
+                            // If coordinates has property of id, add radius value
+                            if(values[id]) {
+                                values[id] += value;
+                            } else {
+                                // Creates a new value
+                                values[id] = value;
+                            }
+                        }
+                    }
+                }
+            }
         };
 
         for(dotQuantity; dotQuantity < jsonData.length; dotQuantity++){
-            newCoordinate();
+            prepareCoordinates();
         }
-
-        // console.log(jsonData.length)
+        render();
+        // console.log(jsonData.length);
         console.log(coordinates);
+        console.log(Object.keys(values).length);
     }
 };
 xmlhttp.open("GET", "../Dataset/testData.json", true);
